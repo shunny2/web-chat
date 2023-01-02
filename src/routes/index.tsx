de8eddec44383295360b252bfe01148e8549d7fa
Alexander Davis <alexander.davis.098@gmail.com>
2023-01-02 20:46:35 -0300

    feat: added new chat page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Home } from "../pages/Home";
-import { User } from "../pages/User";
+import { Chat } from "../pages/Chat";
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
 
@@ -31,10 +31,10 @@ export const AppRoutes = () => {
             ></Route>
             
             <Route
-                path="/users"
+                path="/chat"
                 element={
                     <ProtectedRoute accessBy="authenticated">
-                        <User />
+                        <Chat />
                     </ProtectedRoute>
                 }
             ></Route>
