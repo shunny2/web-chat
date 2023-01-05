e69fef06af9120b6c94d05fc21c032c48f77c00e
Alexander Davis <alexander.davis.098@gmail.com>
2023-01-05 18:48:30 -0300

    refactor: updating styles in the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 export const Home = () => {
     return (
         <div className="flex flex-col gap-4 items-center md:justify-center w-screen h-screen">
-            <h1 className="text-4xl md:text-5xl text-center text-purple-500">Welcome to the <strong className="text-white drop-shadow-[0_5px_5px_rgba(168,85,247,1)] shadow-purple-500">Web Chat</strong>!</h1>
+            <h1 className="text-5xl md:text-6xl text-center text-purple-500">Welcome to the <strong className="text-white drop-shadow-[0_5px_5px_rgba(168,85,247,1)] shadow-purple-500">Web Chat</strong>!</h1>
 
-            <span className="text-lg md:text-xl text-purple-300 text-center">
+            <span className="text-xl md:text-1xl text-purple-300 text-center">
                 Web Chat is a web application to exchange messages in real time between users.
             </span>
         </div>
