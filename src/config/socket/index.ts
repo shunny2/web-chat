880bcfcf0a084e0783bdd03b3925ddf46d2a22db
Alexander Davis <alexander.davis.098@gmail.com>
2023-01-05 14:37:53 -0300

    feat: added event configs

--- src/config/socket/index.ts
+++ src/config/socket/index.ts
@@ -1,5 +1,8 @@
 export const SOCKET_URI = process.env.REACT_APP_SOCKET_URI || "http://localhost:3335";
 
 export const EVENTS = {
-    connection: "connection",
+    connect: "connect",
+    user: "user",
+    message: "chat.message",
+    allMessages: "chat.all.messages"
 };
