3e4d08ca1ddfe61327d5535f6a71677c4925a137
Alexander Davis <alexander.davis.098@gmail.com>
2023-01-06 10:57:10 -0300

    refactor: updating styles on components

--- src/components/shared/TextError/index.tsx
+++ src/components/shared/TextError/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export const TextError = (props: PropsWithChildren) => {
     return (
-        <div className="text-red-500 text-sm">
+        <div className="text-red-500 text-sm font-semibold">
             {props.children}
         </div>
     )
