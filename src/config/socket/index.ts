c7a89b1462c98d0d05d01d7acf1ec2e512878a5b
Alexander Davis <alexander.davis.098@gmail.com>
2023-09-16 23:39:29 -0300

    ✨feat: added new online user event

--- src/config/socket/index.ts
+++ src/config/socket/index.ts
@@ -3,6 +3,7 @@ export const SOCKET_URI = process.env.REACT_APP_SOCKET_URI || "http://localhost:
 export const EVENTS = {
     connect: "connect",
     user: "user",
+    onlineUsers: "online.users",
     message: "chat.message",
     allMessages: "chat.all.messages"
 };
