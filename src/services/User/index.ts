38dfe4dc1d2f5a183b22162a25620ce8f4a38ce5
Alexander Davis <alexander.davis.098@gmail.com>
2023-11-07 12:19:11 -0300

    ✨feat: added new endpoints (forgot password and reset password)

--- src/services/User/index.ts
+++ src/services/User/index.ts
@@ -14,12 +14,42 @@ const create = async (userData: IUser): Promise<void | Error> => {
         if (data)
             return data.id;
 
-        return new Error("Error registering user.")
+        return new Error("Error registering user.");
     } catch (error: any) {
         return new Error((error.response.data as { message: string }).message || "Error registering user.");
     }
 }
 
+const forgotPassword = async (email: string): Promise<void | Error> => {
+    try {
+        const { data } = await jwtInterceptor.post("users/forgot-password", { email });
+        
+        if (data)
+            return data;
+
+        return new Error("Error send reset password email.");
+
+    } catch (error: any) {
+        return new Error((error.response.data as { message: string }).message || "Error send reset password email.");
+    }
+}
+
+const resetPassword = async (token: string | undefined, password: string): Promise<void | Error> => {
+    try {
+        const { data } = await jwtInterceptor.patch(`users/reset-password/${token}`, { password });
+        
+        if (data)
+            return data;
+
+        return new Error("Error when resetting password.");
+
+    } catch (error: any) {
+        return new Error((error.response.data as { message: string }).message || "Error when resetting password.");
+    }
+}
+
 export const UserService = {
-    create
+    create,
+    forgotPassword,
+    resetPassword
 };
